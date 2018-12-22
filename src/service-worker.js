c3a6a661a9e86dfac3672e1df6db3a42623e3612
Sota Hatakeyama <choo.bla.rin@gmail.com>
2018-12-22 21:41:44 +0900

    Remove cache stuffs

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,33 +1,7 @@
-self.addEventListener('install', function (event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-  event.waitUntil(
-    caches.open('v1').then(function (cache) {
-      cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css'
-      ]);
-    })
-  );
+self.addEventListener("install", function(event) {
+  console.log("[Service Worker] Installing Service Worker ...", event);
 });
 
-self.addEventListener('activate', function (event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response !== undefined) {
-        return response;
-      } else {
-        return fetch(event.request).then(function (res) {
-          return caches.open('v1').then(function (cache) {
-            cache.put(event.request, res.clone());
-            return res;
-          });
-        });
-      }
-    })
-  );
+self.addEventListener("activate", function(event) {
+  console.log("[Service Worker] Activating Service Worker ....", event);
 });
