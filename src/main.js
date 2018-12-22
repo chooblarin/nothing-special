ffdf0faaeeaa99d535b876ae88a3b9453c2c5dc5
Sota Hatakeyama <choo.bla.rin@gmail.com>
2018-12-22 21:50:53 +0900

    Style top page

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js')
-    .then(function (registration) {
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then(function(registration) {
       console.log(
-        'Service Worker registration successful with scope: ',
+        "Service Worker registration successful with scope: ",
         registration.scope
       );
     })
-    .catch(function (err) {
-      console.log('Service Worker registration failed: ', err);
+    .catch(function(err) {
+      console.log("Service Worker registration failed: ", err);
     });
 }
